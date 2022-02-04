82d73ff9341fc64c822388886dc4cc95215eb1ef
crowiator <vrana.samuel@gmail.com>
2022-02-04 11:49:17 +0100

    posts comments

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <article class="post">
+            <header>
+                <h2 class=>
+
+                    {{$post->title}}
+                </h2>
+            </header>
+            <div class="content">
+                <img class="img-fluid" alt="Responsive image" src="{{ asset('public/image/post/'.$post->image) }}">
+                <p>{{$post->text}}</p>
+            </div>
+            <footer>
+                <div class="footer_article">
+                    <a href="/users/{{$post->user->id}}" class="author">
+                        <strong>Author: {{$post->user->name}}</strong>
+                    </a>
+                    <!--  -->
+
+
+                </div>
+            </footer>
+
+        </article>
+    </div>
+@endsection
